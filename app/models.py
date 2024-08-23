9dbaab204fb0e6fdcb80e52c1f8687e8926cb89a
nazennash <nazennash120@gmail.com>
2024-08-23 07:49:54 +0300

    pagination-color_model

--- app/models.py
+++ app/models.py
@@ -21,15 +21,15 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     stock = models.IntegerField(default=0)
-    is_sold = models.BooleanField(default=False)
     favorite = models.IntegerField(default=0)
+    color = models.CharField(max_length=255, blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
-    # Starlink Standard Actuated Kit
 
 
 class ProductImage(models.Model):
--- app/views.py
+++ app/views.py
@@ -1,52 +1,88 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Product
+from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
 from django.db.models import Q
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from .models import Product, Category
 from django.utils import timezone
+from rest_framework.pagination import PageNumberPagination
+import random
 
-
-# Create your views here.
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = CustomPageNumberPagination  
 
     def get_queryset(self):
         queryset = super().get_queryset()
+
         search = self.request.query_params.get('search', None)
-        if search is not None:
+        min_price = self.request.query_params.get('min_price', None)
+        max_price = self.request.query_params.get('max_price', None)
+        color = self.request.query_params.get('color', None)
+        category_name = self.request.query_params.get('category', None)
+
+        if search:
             queryset = queryset.filter(
-                Q(name__icontains=search) | 
-                Q(category__name__icontains=search)  
+                Q(name__icontains=search) |
+                Q(description__icontains=search)
             )
-        return queryset
+
+        if category_name:
+            queryset = queryset.filter(category__name__icontains=category_name)
+
+        if min_price and max_price:
+            queryset = queryset.filter(price__gte=min_price, price__lte=max_price)
+        elif min_price:
+            queryset = queryset.filter(price__gte=min_price)
+        elif max_price:
+            queryset = queryset.filter(price__lte=max_price)
+
+        if color:
+            queryset = queryset.filter(color__icontains=color)
+
+        return queryset.order_by('?')
 
     @action(detail=False, methods=['get'])
     def new_arrivals(self, request):
         now = timezone.now()
-        thirty_minutes_ago = now - timezone.timedelta(minutes=30)
-        products = self.queryset.filter(date_created__gte=thirty_minutes_ago).order_by('-date_created')[:4]
-        serializer = self.get_serializer(products, many=True)
-        return Response(serializer.data)
-
-
-    # @action(detail=False, methods=['get'])
-    # def category(self, request):
-    #     category_name = request.query_params.get('category')
+        time_ago = now - timezone.timedelta(days=1)
+        # time_ago = now - timezone.timedelta(hours=4)
+        products = self.queryset.filter(date_created__gte=time_ago)
         
-    #     if category_name is not None:
-    #         products = self.queryset.filter(category__icontains=category_name)
-    #         serializer = self.get_serializer(products, many=True)
-    #         return Response(serializer.data)
-    #     else:
-    #         categories = self.queryset.values_list('category', flat=True).distinct()
-    #         return Response({"categories": categories})
+        products_list = list(products)
+        random.shuffle(products_list)
         
+        paginator = self.pagination_class()
+        paginated_products = paginator.paginate_queryset(products_list, request)
+        serializer = self.get_serializer(paginated_products, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+    @action(detail=False, methods=['get'], url_path='category/(?P<category_name>[^/.]+)')
+    def products_by_category(self, request, category_name=None):
+        products = self.queryset.filter(category__name=category_name)
+        
+        paginator = self.pagination_class()
+        paginated_products = paginator.paginate_queryset(products, request)
+        
+        serializer = self.get_serializer(paginated_products, many=True)
+        
+        return paginator.get_paginated_response(serializer.data)
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    pagination_class = CustomPageNumberPagination  
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        paginator = self.pagination_class()
+        paginated_queryset = paginator.paginate_queryset(queryset, request)
+        serializer = self.get_serializer(paginated_queryset, many=True)
+        return paginator.get_paginated_response(serializer.data)
