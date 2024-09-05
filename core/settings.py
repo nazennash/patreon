60788515ff7295b1001e55f13f854b6827956caa
Nash Oliver <92800845+nazennash@users.noreply.github.com>
2024-09-05 18:19:05 +0300

    media

--- core/settings.py
+++ core/settings.py
@@ -63,7 +63,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
-DISABLE_COLLECTSTATIC = 1
 
 
 # Database
@@ -111,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
