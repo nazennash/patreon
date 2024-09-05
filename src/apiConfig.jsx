73e91c1c52139ca872572b6fba681e2edf4e38f2
Nash Oliver <92800845+nazennash@users.noreply.github.com>
2024-09-05 18:35:07 +0300

    url-change

--- src/apiConfig.jsx
+++ src/apiConfig.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = "https://dolphin-app-betjw.ondigitalocean.app/";
+// const API_URL = "http://localhost:8000";
 
 export const apiConfig = axios.create({
     baseURL: API_URL,
